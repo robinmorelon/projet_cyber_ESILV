3c471a3b107dc329efb03d31a0052ec1cfe48978
morelon231 <morelon231@unicaen.fr>
2026-01-09 11:55:29 +0100

    Rajout de colonne pour le df : voir dataframe.txt

--- fichier_python/pipeline.py
+++ fichier_python/pipeline.py
@@ -12,14 +12,22 @@ liste_lien = [entry.link for entry in rss_feed.entries]
 # Créons une liste avec toutes les CVE de chaque lien
 #lien_test = 'https://www.cert.ssi.gouv.fr/avis/CERTFR-2025-AVI-1076/'
 ref_cves = []
+cve_unique = set()
 for elem in liste_lien:
     url = elem+"json/"
     response = requests.get(url)
     data = response.json()
+    #print(data)
     cves_du_bulletin = data['cves']
     # Cette étape sert à garder l'URL de l'alerte
     for cve in cves_du_bulletin:
         cve['source_alerte'] = elem
+        cve["id_alerte"] = data["reference"]
+        cve["title_alerte"] = data["title"]
+        #Pour être certain que c'est bien la date de publication car il y a plusieurs date
+        if data["revisions"][0]["description"] == "Version initiale":
+            cve["date_publication_alerte"] = data["revisions"][0]["revision_date"]
+        cve["description_alerte"] = data["risks"][0]["description"]
     ref_cves.extend(cves_du_bulletin)
 print(ref_cves)
 
